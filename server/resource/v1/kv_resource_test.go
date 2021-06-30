50a92251fc764b646c7cf47f2ddef227359afdfe
SphaIris <aaalixiaopei123>
2021-06-30 20:58:36 +0800

    fix error

--- server/resource/v1/kv_resource_test.go
+++ server/resource/v1/kv_resource_test.go
@@ -60,7 +60,7 @@ func init() {
 		ListenPeerAddr: "127.0.0.1:4000",
 		AdvertiseAddr:  "127.0.0.1:4000",
 	}
-	config.Configurations.DB.URI = "mongodb://kie:123@192.168.0.5:27017/kie"
+	config.Configurations.DB.URI = "mongodb://kie:123@127.0.0.1:27017/kie"
 	err := service.DBInit()
 	if err != nil {
 		panic(err)
