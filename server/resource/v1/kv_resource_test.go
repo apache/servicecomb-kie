d04fd0d410fd423561e9acee746679857f4d001a
SphaIris <aaalixiaopei123>
2021-08-10 17:19:51 +0800

    add labels

--- server/resource/v1/kv_resource_test.go
+++ server/resource/v1/kv_resource_test.go
@@ -794,8 +794,9 @@ func TestKVResource_PutAndGet(t *testing.T) {
 
 		archaius.Set(qms.QuotaConfigKey, 2)
 		j, _ := json.Marshal(&model.KVDoc{
-			Key:   "reached_quota",
-			Value: "1",
+			Key:    "reached_quota",
+			Value:  "1",
+			Labels: map[string]string{"a": "a"},
 		})
 		r, _ := http.NewRequest("POST", "/v1/kv_test/kie/kv", bytes.NewBuffer(j))
 		r.Header.Set("Content-Type", "application/json")
