24b40d78f892292fc78b39e64fdd0accf19c4a7a
Shawn <xiaoliang.tian@gmail.com>
2021-08-26 09:06:01 +0800

    optimize long polling performance (#210)

--- server/pubsub/event_handler.go
+++ server/pubsub/event_handler.go
@@ -19,34 +19,78 @@ package pubsub
 
 import (
 	"strings"
+	"sync"
+	"time"
 
 	"github.com/go-chassis/openlog"
 	"github.com/hashicorp/serf/serf"
 )
 
-//EventHandler handler serf custom event
+//EventHandler handler serf custom event, it is singleton
 type EventHandler struct {
+	BatchSize          int
+	BatchInterval      time.Duration
+	Immediate          bool
+	pendingEvents      sync.Map
+	pendingEventsCount int
 }
 
 //HandleEvent send event to subscribers
 func (h *EventHandler) HandleEvent(e serf.Event) {
-	openlog.Info("receive event:" + e.EventType().String())
+	openlog.Debug("receive event:" + e.EventType().String())
 	switch e.EventType().String() {
 	case "user":
 		if strings.Contains(e.String(), EventKVChange) {
-			handleKVEvent(e)
+			h.handleKVEvent(e)
 		}
 	}
 
 }
+func (h *EventHandler) RunFlushTask() {
+	for {
+		if h.pendingEventsCount >= h.BatchSize {
+			h.fireEvents()
+		}
+		<-time.After(h.BatchInterval)
+		h.fireEvents()
+	}
 
-func handleKVEvent(e serf.Event) {
+}
+func (h *EventHandler) handleKVEvent(e serf.Event) {
 	ue := e.(serf.UserEvent)
 	ke, err := NewKVChangeEvent(ue.Payload)
 	if err != nil {
 		openlog.Error("invalid json:" + string(ue.Payload))
 	}
 	openlog.Debug("kv event:" + ke.Key)
+	if h.Immediate { //never retain event, not recommended
+		h.FindTopicAndFire(ke)
+	} else {
+		h.mergeAndSave(ke)
+	}
+
+}
+func (h *EventHandler) mergeAndSave(ke *KVChangeEvent) {
+	id := ke.String()
+	_, ok := h.pendingEvents.Load(id)
+	if ok {
+		openlog.Debug("ignore same event: " + id)
+		return
+	}
+	h.pendingEvents.Store(id, ke)
+	h.pendingEventsCount++
+}
+func (h *EventHandler) fireEvents() {
+	h.pendingEvents.Range(func(key, value interface{}) bool {
+		ke := value.(*KVChangeEvent)
+		h.FindTopicAndFire(ke)
+		h.pendingEvents.Delete(key)
+		h.pendingEventsCount--
+		return true
+	})
+}
+
+func (h *EventHandler) FindTopicAndFire(ke *KVChangeEvent) {
 	topics.Range(func(key, value interface{}) bool { //range all topics
 		t, err := ParseTopicString(key.(string))
 		if err != nil {
