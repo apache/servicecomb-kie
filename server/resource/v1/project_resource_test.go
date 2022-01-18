5b0cbb3c91982c0e37cecb3a7462f9ee3435ca37
ShengTao <taosheng@corp-ci.com>
2022-01-18 10:23:34 +0800

    feat(*): add test

--- server/resource/v1/project_resource_test.go
+++ server/resource/v1/project_resource_test.go
@@ -18,7 +18,37 @@
 package v1_test
 
 import (
+	"bytes"
+	"encoding/json"
+	"github.com/apache/servicecomb-kie/pkg/model"
 	_ "github.com/apache/servicecomb-kie/server/datasource/mongo"
 	_ "github.com/apache/servicecomb-kie/server/plugin/qms"
+	v1 "github.com/apache/servicecomb-kie/server/resource/v1"
 	_ "github.com/apache/servicecomb-kie/test"
+	"github.com/go-chassis/go-chassis/v2/server/restful/restfultest"
+	"github.com/stretchr/testify/assert"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"testing"
 )
+
+func TestProjectResource_Get(t *testing.T) {
+	t.Run("get project list", func(t *testing.T) {
+		kv := &model.ProjectDoc{}
+		j, _ := json.Marshal(kv)
+		r, _ := http.NewRequest("GET", "/v1/project", bytes.NewBuffer(j))
+		r.Header.Set("Content-Type", "application/json")
+		pr := &v1.ProjectResource{}
+		c, _ := restfultest.New(pr, nil)
+		resp := httptest.NewRecorder()
+		c.ServeHTTP(resp, r)
+		body, err := ioutil.ReadAll(resp.Body)
+		assert.NoError(t, err)
+		assert.Equal(t, http.StatusOK, resp.Code, string(body))
+		result := &model.ProjectResponse{}
+		err = json.Unmarshal(body, result)
+		assert.NoError(t, err)
+		assert.Equal(t, 3, len(result.Data))
+	})
+}
