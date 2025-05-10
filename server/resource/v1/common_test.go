8ae4c6f644ec4bfc99475ce2fe04ef51c5f87b10
humingcheng <humingcheng1990@163.com>
2025-05-11 01:54:46 +0800

    Wait时间即将耗尽时，忽略超时报错，降低误报错概率

--- server/resource/v1/common_test.go
+++ server/resource/v1/common_test.go
@@ -15,15 +15,16 @@
  * limitations under the License.
  */
 
-package v1_test
+package v1
 
 import (
+	"errors"
+	"github.com/apache/servicecomb-kie/server/pubsub"
+	_ "github.com/apache/servicecomb-kie/test"
 	"net/http"
 	"testing"
+	"time"
 
-	_ "github.com/apache/servicecomb-kie/test"
-
-	v1 "github.com/apache/servicecomb-kie/server/resource/v1"
 	"github.com/emicklei/go-restful"
 	"github.com/stretchr/testify/assert"
 )
@@ -33,7 +34,7 @@ func TestGetLabels(t *testing.T) {
 		"/kv?q=app:mall+service:payment&q=app:mall+service:payment+version:1.0.0",
 		nil)
 	assert.NoError(t, err)
-	c, err := v1.ReadLabelCombinations(restful.NewRequest(r))
+	c, err := ReadLabelCombinations(restful.NewRequest(r))
 	assert.NoError(t, err)
 	assert.Equal(t, 2, len(c))
 
@@ -41,7 +42,94 @@ func TestGetLabels(t *testing.T) {
 		"/kv",
 		nil)
 	assert.NoError(t, err)
-	c, err = v1.ReadLabelCombinations(restful.NewRequest(r))
+	c, err = ReadLabelCombinations(restful.NewRequest(r))
 	assert.NoError(t, err)
 	assert.Equal(t, 1, len(c))
+}
+
+func Test_splitWaitInterval(t *testing.T) {
+	type args struct {
+		d time.Duration
+	}
+	tests := []struct {
+		name                          string
+		args                          args
+		wantNormalInterval            time.Duration
+		wantTimeoutProtectionInterval time.Duration
+	}{
+		{
+			name: "regular interval",
+			args: args{
+				d: 5 * time.Second,
+			},
+			wantNormalInterval:            4 * time.Second,
+			wantTimeoutProtectionInterval: 1 * time.Second,
+		},
+		{
+			name: "interval very big, origin timeout protect bigger than the max value",
+			args: args{
+				d: 5000 * time.Second,
+			},
+			wantNormalInterval:            4998 * time.Second,
+			wantTimeoutProtectionInterval: 2 * time.Second,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			gotNormalInterval, gotTimeoutProtectionInterval := splitWaitInterval(tt.args.d)
+			assert.Equalf(t, tt.wantNormalInterval, gotNormalInterval, "splitWaitInterval(%v)", tt.args.d)
+			assert.Equalf(t, tt.wantTimeoutProtectionInterval, gotTimeoutProtectionInterval, "splitWaitInterval(%v)", tt.args.d)
+		})
+	}
+}
+
+func getObserverThatHappenedEventAfter10ms() *pubsub.Observer {
+	observer, _ := NewObserver()
+	go func() {
+		time.Sleep(10 * time.Millisecond)
+		observer.Event <- &pubsub.KVChangeEvent{}
+	}()
+	return observer
+}
+
+func Test_waitObserverEventHappened(t *testing.T) {
+	// timeout
+	assert.False(t, waitObserverEventHappened(getObserverThatHappenedEventAfter10ms(), 5*time.Millisecond))
+
+	// not timeout
+	assert.True(t, waitObserverEventHappened(getObserverThatHappenedEventAfter10ms(), 50*time.Millisecond))
+
+	// zero interval
+	assert.False(t, waitObserverEventHappened(getObserverThatHappenedEventAfter10ms(), 0*time.Millisecond))
+}
+
+func Test_isDeadlineExceededErr(t *testing.T) {
+	type args struct {
+		err error
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		{
+			name: "DeadlineExceededErr",
+			args: args{
+				err: errors.New("error is context deadline exceeded"),
+			},
+			want: true,
+		},
+		{
+			name: "no DeadlineExceededErr",
+			args: args{
+				err: errors.New("error is unknown"),
+			},
+			want: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			assert.Equalf(t, tt.want, isDeadlineExceededErr(tt.args.err), "isDeadlineExceededErr(%v)", tt.args.err)
+		})
+	}
 }
