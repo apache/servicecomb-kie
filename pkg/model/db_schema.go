cd8caf05dde6181d1ded838910025643c7dad33c
holden-cpu <67038342+holden-cpu@users.noreply.github.com>
2025-09-02 17:38:48 +0800

    Update db_schema.go ListKVRequest key的检验长度修改为256

--- pkg/model/db_schema.go
+++ pkg/model/db_schema.go
@@ -95,7 +95,7 @@ type GetKVRequest struct {
 type ListKVRequest struct {
 	Project string            `json:"project,omitempty" yaml:"project,omitempty" validate:"min=1,max=256,commonName"`
 	Domain  string            `json:"domain,omitempty" yaml:"domain,omitempty" validate:"min=1,max=256,commonName"` //redundant
-	Key     string            `json:"key" yaml:"key" validate:"max=128,getKey"`
+	Key     string            `json:"key" yaml:"key" validate:"max=256,getKey"`
 	Value   string            `json:"value" yaml:"value" validate:"max=128"`
 	Labels  map[string]string `json:"labels,omitempty" yaml:"labels,omitempty" validate:"max=8,dive,keys,labelK,endkeys,labelV"` //redundant
 	Offset  int64             `validate:"min=0"`
