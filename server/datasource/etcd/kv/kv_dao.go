43107b9d825feda75dc26a7120db5e846b831ff2
humingcheng <humingcheng1990@163.com>
2026-01-05 22:45:12 +0800

    Revert "修复模糊匹配缓存数据设置错误"

--- server/datasource/etcd/kv/kv_dao.go
+++ server/datasource/etcd/kv/kv_dao.go
@@ -550,7 +550,7 @@ func (s *Dao) listData(ctx context.Context, project, domain string, options ...d
 		return listDataByCache(ctx, project, domain, opts, regex)
 	}
 
-	result, _, err := listDataByNoCache(ctx, domain, project, regex, opts)
+	result, err := matchLabelsSearch(ctx, domain, project, regex, opts)
 	if err != nil {
 		openlog.Error("list kv failed: " + err.Error())
 		return nil, opts, err
@@ -585,7 +585,7 @@ func listDataByCache(ctx context.Context, project string, domain string, opts da
 		openlog.Info("using fuzzy cache to search kv not hit")
 	}
 
-	result, onlyLabelFilteredResult, err := listDataByNoCache(ctx, domain, project, regex, opts)
+	result, err := matchLabelsSearch(ctx, domain, project, regex, opts)
 	if err != nil {
 		openlog.Error("list kv failed: " + err.Error())
 		return nil, opts, err
@@ -596,30 +596,23 @@ func listDataByCache(ctx context.Context, project string, domain string, opts da
 	}
 
 	kvIdSet := new(sync.Map)
-	for _, kv := range onlyLabelFilteredResult.Data {
+	for _, kv := range result.Data {
 		kvIdSet.Store(kv.ID, struct{}{})
 	}
 	cacheKey := kvCache.GetCacheKey(domain, project, opts.Labels)
-	kvCache.kvIDFuzzyCache.Set(cacheKey, kvIdSet, int64(onlyLabelFilteredResult.Total))
+	kvCache.kvIDFuzzyCache.Set(cacheKey, kvIdSet, int64(result.Total))
 	return result, opts, nil
 }
 
-// 返回值 onlyLabelFilteredResult 用于调用者设置缓存
-func listDataByNoCache(ctx context.Context, domain, project string, regex *regexp.Regexp,
-	opts datasource.FindOptions) (result, onlyLabelFilteredResult *model.KVResponse, err error) {
+func matchLabelsSearch(ctx context.Context, domain, project string, regex *regexp.Regexp, opts datasource.FindOptions) (*model.KVResponse, error) {
 	openlog.Debug("using labels to search kv")
 	kvs, _, err := etcdadpt.List(ctx, key.KVList(domain, project))
 	if err != nil {
-		return nil, nil, err
+		return nil, err
 	}
-
-	result = &model.KVResponse{
+	result := &model.KVResponse{
 		Data: []*model.KVDoc{},
 	}
-	onlyLabelFilteredResult = &model.KVResponse{
-		Data: []*model.KVDoc{},
-	}
-
 	for _, kv := range kvs {
 		var doc model.KVDoc
 		err := json.Unmarshal(kv.Value, &doc)
@@ -627,21 +620,17 @@ func listDataByNoCache(ctx context.Context, domain, project string, regex *regex
 			openlog.Error("decode to KVList error: " + err.Error())
 			continue
 		}
-		if !matchLabels(&doc, opts) {
+		if !filterMatch(&doc, opts, regex) {
 			continue
 		}
-		datasource.ClearPart(&doc)
-		onlyLabelFilteredResult.Data = append(onlyLabelFilteredResult.Data, &doc)
-		onlyLabelFilteredResult.Total++
 
-		if !matchConditions(&doc, opts, regex) {
-			continue
-		}
+		datasource.ClearPart(&doc)
 		result.Data = append(result.Data, &doc)
 		result.Total++
+
 	}
 
-	return result, onlyLabelFilteredResult, nil
+	return result, nil
 }
 
 func IsUniqueFind(opts datasource.FindOptions) bool {
@@ -693,7 +682,13 @@ func pagingResult(result *model.KVResponse, opts datasource.FindOptions) *model.
 	return result
 }
 
-func matchLabels(doc *model.KVDoc, opts datasource.FindOptions) bool {
+func filterMatch(doc *model.KVDoc, opts datasource.FindOptions, regex *regexp.Regexp) bool {
+	if opts.Status != "" && doc.Status != opts.Status {
+		return false
+	}
+	if regex != nil && !regex.MatchString(doc.Key) {
+		return false
+	}
 	if len(opts.Labels) != 0 {
 		if opts.ExactLabels && !util.IsEquivalentLabel(opts.Labels, doc.Labels) {
 			return false
@@ -702,18 +697,6 @@ func matchLabels(doc *model.KVDoc, opts datasource.FindOptions) bool {
 			return false
 		}
 	}
-
-	return true
-}
-
-func matchConditions(doc *model.KVDoc, opts datasource.FindOptions, regex *regexp.Regexp) bool {
-	if opts.Status != "" && doc.Status != opts.Status {
-		return false
-	}
-	if regex != nil && !regex.MatchString(doc.Key) {
-		return false
-	}
-
 	if opts.LabelFormat != "" && doc.LabelFormat != opts.LabelFormat {
 		return false
 	}
